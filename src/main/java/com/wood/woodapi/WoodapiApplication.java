a7348774159e460e53aed859e6788ed6be3f25c9
wood <wood13590@gmail.com>
2024-08-29 19:47:44 +0800

    add Dubbo 整合, 实现基本调用

--- src/main/java/com/wood/woodapi/WoodapiApplication.java
+++ src/main/java/com/wood/woodapi/WoodapiApplication.java
@@ -1,11 +1,16 @@
 package com.wood.woodapi;
 
+import javafx.application.Application;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.io.File;
 
 /**
  * 主类（项目启动入口）
@@ -16,9 +21,27 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @MapperScan("com.wood.woodapi.mapper")
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+@EnableDubbo
 public class WoodapiApplication {
 
     public static void main(String[] args) {
+        // 创建SpringApplication实例
+        SpringApplication application = new SpringApplication(Application.class);
+        // 添加自定义的ApplicationContextInitializer
+        application.addInitializers(context -> {
+            // 获取Environment对象
+            Environment env = context.getEnvironment();
+            // 从Environment中读取"spring.application.name"属性值
+//            String appName = env.getProperty("spring.application.name");
+            String appName = "woodapi";
+            String filePath = System.getProperty("user.home") + File.separator + ".dubbo" + File.separator + appName;
+            // 修改dubbo的本地缓存路径，避免缓存冲突
+            System.setProperty("dubbo.meta.cache.filePath.meta", filePath);
+            System.setProperty("dubbo.mapping.cache.filePath.mapping", filePath);
+        });
+        // 启动应用
+//        SpringApplication.run(application.getClass(), args);
+//        application.run(WoodapiApplication.class, args);
         SpringApplication.run(WoodapiApplication.class, args);
     }
 
--- src/test/java/com/wood/woodapi/MainApplicationTests.java
+++ src/test/java/com/wood/woodapi/MainApplicationTests.java
@@ -1,5 +1,9 @@
 package com.wood.woodapi;
 
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.DigestUtils;
@@ -15,5 +19,14 @@ class MainApplicationTests {
     void contextLoads() {
         String encryptPassword = DigestUtils.md5DigestAsHex(("wood12345678").getBytes());
         System.out.println(encryptPassword);
+    }
+
+    @Test
+    void test() throws NacosException {
+        String serverAddr = "localhost:8848";
+
+        ConfigService configService = NacosFactory.createConfigService(serverAddr);
+
+        NamingService namingService = NacosFactory.createNamingService(serverAddr);
     }
 }
