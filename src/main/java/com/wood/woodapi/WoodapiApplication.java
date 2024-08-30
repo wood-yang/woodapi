2cb36a5275996bf9aa28a062e52493e019c59b1f
wood <wood13590@gmail.com>
2024-08-30 09:12:47 +0800

    update 实现三个公共接口

--- src/main/java/com/wood/woodapi/WoodapiApplication.java
+++ src/main/java/com/wood/woodapi/WoodapiApplication.java
@@ -25,23 +25,8 @@ import java.io.File;
 public class WoodapiApplication {
 
     public static void main(String[] args) {
-        // 创建SpringApplication实例
-        SpringApplication application = new SpringApplication(Application.class);
-        // 添加自定义的ApplicationContextInitializer
-        application.addInitializers(context -> {
-            // 获取Environment对象
-            Environment env = context.getEnvironment();
-            // 从Environment中读取"spring.application.name"属性值
-//            String appName = env.getProperty("spring.application.name");
-            String appName = "woodapi";
-            String filePath = System.getProperty("user.home") + File.separator + ".dubbo" + File.separator + appName;
-            // 修改dubbo的本地缓存路径，避免缓存冲突
-            System.setProperty("dubbo.meta.cache.filePath.meta", filePath);
-            System.setProperty("dubbo.mapping.cache.filePath.mapping", filePath);
-        });
-        // 启动应用
-//        SpringApplication.run(application.getClass(), args);
-//        application.run(WoodapiApplication.class, args);
+        System.setProperty("dubbo.meta.cache.filePath.meta", "C:\\Users\\24420\\.dubbo\\backend");
+        System.setProperty("dubbo.mapping.cache.filePath.meta", "C:\\Users\\24420\\.dubbo\\backend");
         SpringApplication.run(WoodapiApplication.class, args);
     }
 
--- src/main/java/com/wood/woodapi/provider/impl/InnerUserInterfaceInfoServiceImpl.java
+++ src/main/java/com/wood/woodapi/provider/impl/InnerUserInterfaceInfoServiceImpl.java
@@ -1,15 +1,23 @@
 package com.wood.woodapi.provider.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.wood.common.model.entity.UserInterfaceInfo;
 import com.wood.common.service.InnerUserInterfaceInfoService;
+import com.wood.woodapi.mapper.UserInterfaceInfoMapper;
 import com.wood.woodapi.service.UserInterfaceInfoService;
 import org.apache.dubbo.config.annotation.DubboService;
 
 import javax.annotation.Resource;
+import java.util.List;
+
 @DubboService
 public class InnerUserInterfaceInfoServiceImpl implements InnerUserInterfaceInfoService {
 
     @Resource
     UserInterfaceInfoService userInterfaceInfoService;
+
+    @Resource
+    UserInterfaceInfoMapper userInterfaceInfoMapper;
 
     /**
      * 调用成功，接口调用次数 +1 invokeCount (ak，sk (标识用户)，请求接口路径)
@@ -20,5 +28,20 @@ public class InnerUserInterfaceInfoServiceImpl implements InnerUserInterfaceInfo
     @Override
     public boolean invokeCount(Long userId, Long interfaceInfoId) {
         return userInterfaceInfoService.invokeCount(userId, interfaceInfoId);
+    }
+
+    @Override
+    public boolean isEnough(Long userId, Long interfaceInfoId) {
+        QueryWrapper<UserInterfaceInfo> queryWrapper = new QueryWrapper<>();
+
+        List<UserInterfaceInfo> userInterfaceInfoList = userInterfaceInfoMapper.selectList(queryWrapper);
+        if (userInterfaceInfoList == null) {
+            return false;
+        }
+        UserInterfaceInfo userInterfaceInfo = userInterfaceInfoList.get(0);
+        if (userInterfaceInfo == null || userInterfaceInfo.getLeftNum() <= 0 || userInterfaceInfo.getTotalNum() > 1000) {
+            return false;
+        }
+        return true;
     }
 }
